e291c67f4693a209a3fbf4576be265c413f21e50
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-05 11:29:44 -0500

    Solve problem 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //Formula to convert f to c is ((f-32)*5)/9
+  return parseFloat((((fahrenheit-32)*5)/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //Formula to convert c to f is (c * 9/5)+32
+  return parseFloat(((celsius * 9/5)+32).toFixed(1));
 };
 
 // Do not edit below this line
