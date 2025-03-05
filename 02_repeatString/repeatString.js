434b192a0ce200907943840ee6c22bf7f4853c22
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-04 20:08:53 -0500

    Solve problem 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    if (numberOfRepeats<0) return "ERROR"
+    updatedString = ""
+    for (let i=0; i<numberOfRepeats; i++){
+        updatedString += string
+    }
+    return updatedString
 };
 
 // Do not edit below this line
