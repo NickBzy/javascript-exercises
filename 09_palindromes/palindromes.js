523da6efde00609a1169a066c2145b0e36d7eddc
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-05 15:40:56 -0500

    Solve problem 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9]/g, "");
+    reversed = []
+    for (let i=string.length-1; i>=0; i--){
+        reversed.push(string[i]);
+    }
+    reversedString = reversed.join("").toLowerCase();
+    return string.toLowerCase() === reversedString;
 };
 
 // Do not edit below this line
