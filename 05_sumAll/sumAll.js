a77e7ef019d1c7b6d35fa9ced3710879084a6994
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-05 10:58:49 -0500

    Solve problem 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if ((start<0 || end<0) || 
+    (!Number.isInteger(start) || !Number.isInteger(end))) {
+        return "ERROR";
+    }
+    numberElements = Math.abs(end-start)+1
+    return (numberElements/2 * (start+end));
 };
 
 // Do not edit below this line
