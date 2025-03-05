36740314c18c972ccda4164f5db16fed20ea8868
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-05 16:06:40 -0500

    Solve problem 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n, memo= {}) {
+    n = Number(n)
+    if (n<0) return "OOPS"
+    if (n === 0) return 0
+    if (n===1 || n ===2){
+        return 1
+    }
+    if (memo[n]) return memo[n];
+    memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo);
+    return memo[n];
 };
 
 // Do not edit below this line
