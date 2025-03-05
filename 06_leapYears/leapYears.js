8277eafd5eb31d401dea44a91a8a4a98633ce077
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-05 11:17:13 -0500

    Solve problem 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisbleByFourHundred = year % 400 === 0;
+    //A year is considered a leap year if it's:
+    //divisible by 4 and 
+    //not a century unless 
+    //if it's divisible by 400
+    return (isDivisibleByFour && !isCentury || isDivisbleByFourHundred)
 };
 
 // Do not edit below this line
